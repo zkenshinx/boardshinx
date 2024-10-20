54de18546100a063e3f3e5f55da3fff7d98c33c1
Koba Karaputadze <kobakaraputadze701@gmail.com>
2024-10-20 18:25:46 +0400

    improve join room

--- src/join_room.py
+++ src/join_room.py
@@ -16,6 +16,14 @@ class JoinRoom(BoardState):
     BUTTON_Y = 400
     COLOR_BOX_SIZE = 80
     COLOR_Y_OFFSET = 520
+    COLORS = {
+        "inactive": (150, 150, 150),
+        "hover": (180, 180, 180),
+        "default": (200, 200, 200),
+        "button_active": (0, 100, 0),
+        "button_default": (0, 128, 0),
+        "error": (255, 0, 0)
+    }
 
     def __init__(self, state_manager, data=None):
         super().__init__(state_manager)
@@ -31,7 +39,6 @@ class JoinRoom(BoardState):
         self.join_button_rect = pygame.Rect(0, 0, self.BUTTON_W, self.BUTTON_H)
         self.show_colors = False
         self.available_colors = []
-
         self.assigned_color = None
 
         self.tcp_client = TCPClient()
@@ -74,6 +81,19 @@ class JoinRoom(BoardState):
             self.available_colors = message.get("colors", [])
             return False
 
+    def handle_text_input(self, event, text, max_length):
+        if event.key == pygame.K_BACKSPACE:
+            return text[:-1]
+        elif len(text) < max_length and event.unicode.isascii():
+            return text + event.unicode
+        return text
+
+    def handle_tab(self):
+        if self.input_active[0]:
+            self.input_active = [False, True]
+        else:
+            self.input_active = [True, False]
+
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -81,28 +101,27 @@ class JoinRoom(BoardState):
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.show_colors:
-                    for i, color in enumerate(self.available_colors):
-                        box_x = (self.screen.get_width() - self.COLOR_BOX_SIZE * 3 - 10) // 2 + (i % 3) * (self.COLOR_BOX_SIZE + 10)
-                        box_y = self.COLOR_Y_OFFSET + (i // 3) * (self.COLOR_BOX_SIZE + 10)
-                        box_rect = pygame.Rect(box_x, box_y, self.COLOR_BOX_SIZE, self.COLOR_BOX_SIZE)
-                        if box_rect.collidepoint(event.pos):
-                            self.color_chosen(color)
+                    self.handle_color_selection(event.pos)
                 elif self.join_button_rect.collidepoint(event.pos):
                     self.find_room()
                 else:
                     self.input_active[0] = pygame.Rect((self.screen.get_width() - self.INPUT_W) // 2, self.ROOM_INPUT_Y, self.INPUT_W, self.INPUT_H).collidepoint(event.pos)
                     self.input_active[1] = pygame.Rect((self.screen.get_width() - self.INPUT_W) // 2, self.NAME_INPUT_Y, self.INPUT_W, self.INPUT_H).collidepoint(event.pos)
             if event.type == pygame.KEYDOWN:
-                if self.input_active[0]:
-                    if event.key == pygame.K_BACKSPACE:
-                        self.room_code = self.room_code[:-1]
-                    elif len(self.room_code) < 6:
-                        self.room_code += event.unicode
+                if event.key == pygame.K_TAB:
+                    self.handle_tab()
+                elif self.input_active[0]:
+                    self.room_code = self.handle_text_input(event, self.room_code, 6)
                 elif self.input_active[1]:
-                    if event.key == pygame.K_BACKSPACE:
-                        self.user_name = self.user_name[:-1]
-                    else:
-                        self.user_name += event.unicode
+                    self.user_name = self.handle_text_input(event, self.user_name, 10)
+
+    def handle_color_selection(self, mouse_pos):
+        for i, color in enumerate(self.available_colors):
+            box_x = (self.screen.get_width() - self.COLOR_BOX_SIZE * 3 - 10) // 2 + (i % 3) * (self.COLOR_BOX_SIZE + 10)
+            box_y = self.COLOR_Y_OFFSET + (i // 3) * (self.COLOR_BOX_SIZE + 10)
+            box_rect = pygame.Rect(box_x, box_y, self.COLOR_BOX_SIZE, self.COLOR_BOX_SIZE)
+            if box_rect.collidepoint(mouse_pos):
+                self.color_chosen(color)
 
     def color_chosen(self, color):
         self.tcp_client.send({
@@ -113,10 +132,7 @@ class JoinRoom(BoardState):
     def find_room(self):
         self.user_name = self.user_name.lower()
 
-        if len(self.room_code) == 0:
-            return
-
-        if not self.user_name.isalpha() or len(self.user_name) == 0:
+        if len(self.room_code) == 0 or not self.user_name.isalpha():
             return
 
         self.tcp_client.send({
@@ -127,61 +143,57 @@ class JoinRoom(BoardState):
 
     def draw(self):
         self.screen.fill("#FFFFFF")
-
         mouse_pos = pygame.mouse.get_pos()
 
-        # Draw Room Label
-        room_label_surface = self.label_font.render("Room:", True, (0, 0, 0))
-        self.screen.blit(room_label_surface, ((self.screen.get_width() - self.INPUT_W) // 2 - self.LABEL_OFFSET, self.ROOM_LABEL_Y))
+        self.draw_label("Room:", (self.screen.get_width() - self.INPUT_W) // 2 - self.LABEL_OFFSET, self.ROOM_LABEL_Y)
+        self.draw_input_box(self.room_code, (self.screen.get_width() - self.INPUT_W) // 2, self.ROOM_INPUT_Y, self.input_active[0], mouse_pos)
 
-        # Draw Room Code Input Box
-        room_code_rect = pygame.Rect((self.screen.get_width() - self.INPUT_W) // 2, self.ROOM_INPUT_Y, self.INPUT_W, self.INPUT_H)
-        room_code_color = (180, 180, 180) if room_code_rect.collidepoint(mouse_pos) else (200, 200, 200)
-        self.draw_rounded_rect(room_code_rect, room_code_color, 10)
-        room_code_surface = self.font.render(self.room_code, True, (0, 0, 0))
-        text_rect = room_code_surface.get_rect(center=room_code_rect.center)
-        self.screen.blit(room_code_surface, text_rect)
+        self.draw_label("Name:", (self.screen.get_width() - self.INPUT_W) // 2 - self.LABEL_OFFSET, self.NAME_LABEL_Y)
+        self.draw_input_box(self.user_name, (self.screen.get_width() - self.INPUT_W) // 2, self.NAME_INPUT_Y, self.input_active[1], mouse_pos)
 
-        # Draw Name Label
-        name_label_surface = self.label_font.render("Name:", True, (0, 0, 0))
-        self.screen.blit(name_label_surface, ((self.screen.get_width() - self.INPUT_W) // 2 - self.LABEL_OFFSET, self.NAME_LABEL_Y))
+        self.draw_button("Find", self.join_button_rect, self.BUTTON_Y, mouse_pos)
 
-        # Draw User Name Input Box
-        user_name_rect = pygame.Rect((self.screen.get_width() - self.INPUT_W) // 2, self.NAME_INPUT_Y, self.INPUT_W, self.INPUT_H)
-        user_name_color = (180, 180, 180) if user_name_rect.collidepoint(mouse_pos) else (200, 200, 200)
-        self.draw_rounded_rect(user_name_rect, user_name_color, 10)
-        user_name_surface = self.font.render(self.user_name, True, (0, 0, 0))
-        text_rect = user_name_surface.get_rect(center=user_name_rect.center)
-        self.screen.blit(user_name_surface, text_rect)
-
-        # Draw Find Button
-        self.join_button_rect.topleft = ((self.screen.get_width() - self.BUTTON_W) // 2, self.BUTTON_Y)
-        button_color = (0, 100, 0) if self.join_button_rect.collidepoint(mouse_pos) else (0, 128, 0)
-        self.draw_rounded_rect(self.join_button_rect, button_color, 10)
-        button_text_surface = self.font.render("Find", True, (255, 255, 255))
-        text_rect = button_text_surface.get_rect(center=self.join_button_rect.center)
-        self.screen.blit(button_text_surface, text_rect)
-
-        # Draw Error Message if any
         if hasattr(self, 'error_message'):
-            error_surface = self.label_font.render(self.error_message, True, (255, 0, 0))
-            error_rect = error_surface.get_rect(center=(self.screen.get_width() // 2, self.BUTTON_Y + 70))
-            self.screen.blit(error_surface, error_rect)
+            self.draw_error_message()
 
         if self.show_colors:
-            # Draw color selection text
-            color_text_surface = self.color_label_font.render("Choose your color:", True, (0, 0, 0))
-            color_text_rect = color_text_surface.get_rect(center=(self.screen.get_width() // 2, self.COLOR_Y_OFFSET - 30))
-            self.screen.blit(color_text_surface, color_text_rect)
-
-            # Draw Color Boxes
-            for i, color in enumerate(self.available_colors):
-                box_x = (self.screen.get_width() - self.COLOR_BOX_SIZE * 3 - 10) // 2 + (i % 3) * (self.COLOR_BOX_SIZE + 10)
-                box_y = self.COLOR_Y_OFFSET + (i // 3) * (self.COLOR_BOX_SIZE + 10)
-                box_rect = pygame.Rect(box_x, box_y, self.COLOR_BOX_SIZE, self.COLOR_BOX_SIZE)
-                pygame.draw.rect(self.screen, color, box_rect)
+            self.draw_color_boxes()
 
         pygame.display.flip()
+
+    def draw_label(self, text, x, y):
+        label_surface = self.label_font.render(text, True, (0, 0, 0))
+        self.screen.blit(label_surface, (x, y))
+
+    def draw_input_box(self, text, x, y, active, mouse_pos):
+        input_rect = pygame.Rect(x, y, self.INPUT_W, self.INPUT_H)
+        input_color = self.COLORS["inactive"] if active else self.COLORS["hover"] if input_rect.collidepoint(mouse_pos) else self.COLORS["default"]
+        self.draw_rounded_rect(input_rect, input_color, 10)
+        input_surface = self.font.render(text, True, (0, 0, 0))
+        self.screen.blit(input_surface, input_surface.get_rect(center=input_rect.center))
+
+    def draw_button(self, text, button_rect, y, mouse_pos):
+        button_rect.topleft = ((self.screen.get_width() - self.BUTTON_W) // 2, y)
+        button_color = self.COLORS["button_active"] if button_rect.collidepoint(mouse_pos) else self.COLORS["button_default"]
+        self.draw_rounded_rect(button_rect, button_color, 10)
+        button_surface = self.font.render(text, True, (255, 255, 255))
+        self.screen.blit(button_surface, button_surface.get_rect(center=button_rect.center))
+
+    def draw_error_message(self):
+        error_surface = self.label_font.render(self.error_message, True, self.COLORS["error"])
+        error_rect = error_surface.get_rect(center=(self.screen.get_width() // 2, self.BUTTON_Y + 70))
+        self.screen.blit(error_surface, error_rect)
+
+    def draw_color_boxes(self):
+        color_text_surface = self.color_label_font.render("Choose your color:", True, (0, 0, 0))
+        color_text_rect = color_text_surface.get_rect(center=(self.screen.get_width() // 2, self.COLOR_Y_OFFSET - 30))
+        self.screen.blit(color_text_surface, color_text_rect)
+
+        for i, color in enumerate(self.available_colors):
+            box_x = (self.screen.get_width() - self.COLOR_BOX_SIZE * 3 - 10) // 2 + (i % 3) * (self.COLOR_BOX_SIZE + 10)
+            box_y = self.COLOR_Y_OFFSET + (i // 3) * (self.COLOR_BOX_SIZE + 10)
+            box_rect = pygame.Rect(box_x, box_y, self.COLOR_BOX_SIZE, self.COLOR_BOX_SIZE)
+            pygame.draw.rect(self.screen, color, box_rect)
 
     def draw_rounded_rect(self, rect, color, radius):
         pygame.draw.rect(self.screen, color, rect, border_radius=radius)
